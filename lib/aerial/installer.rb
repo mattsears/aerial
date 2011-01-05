74f87a719e561007671d31dd6c4d24ce9132b4a4
Matt Sears <matt@mattsears.com>
2011-01-05 09:27:06 -0500

    Added 'server' option to fire up application with Thin

--- lib/aerial/installer.rb
+++ lib/aerial/installer.rb
@@ -5,12 +5,13 @@ module Aerial
 
   class Installer < Thor
     include FileUtils
+    map "-T" => :list
 
     desc "install [PATH]",
     "Copy template files to PATH for desired deployement strategy
        (either Thin or Passenger). Next, go there and edit them."
     method_options :passenger => :boolean, :thin  => :boolean
-    def install(path)
+    def install(path = '.')
       @root = Pathname(path).expand_path
       create_dir_structure
       copy_config_files
@@ -20,16 +21,33 @@ module Aerial
     end
 
     desc "launch [CONFIG]", "Launch Aerial cartwheel."
-    method_options :config => :optional, :port => 4567
-    def launch
+    method_options :config => :optional, :port => 3000
+    def server
+      require 'thin'
       @root = Pathname(".").expand_path
-      require "thin"
-      Aerial.new(@root, "/config/config.yml")
+      Aerial.new(@root, "/config.yml")
       Aerial::App.set :root, @root
       Thin::Server.start("0.0.0.0", options[:port], Aerial::App)
     rescue LoadError => boom
       missing_dependency = boom.message.split("--").last.lstrip
       puts "Please install #{missing_dependency} to launch Aerial"
+    end
+
+    desc "overrides", "Allow aerial to be customized"
+    def overrides
+      @root = Pathname(".").expand_path
+      create_app_file
+    end
+
+    desc "import", "Import articles from another blog."
+    method_option :articles, :type => :hash, :default => {}, :required => true
+    def import
+      @root = Pathname(".").expand_path
+      Aerial.new(@root, "/config.yml")
+      Aerial::Migrator.new(options).process!
+    rescue LoadError => boom
+      missing_dependency = boom.message.split("--").last.lstrip
+      puts "Please install #{missing_dependency} to import article into Aerial"
     end
 
     private
@@ -53,10 +71,7 @@ module Aerial
     # Copy over all files need to run the app
     def bootstrap
       copy "views", "../../examples"
-      copy "public", "../../examples"
-      initialize_repo
       create_initial_article
-      initial_commit
     end
 
     def create_initial_article
@@ -80,13 +95,11 @@ module Aerial
     def copy_config_files
       copy "config.sample.ru"
       copy "config.sample.yml"
-      copy "thin.sample.yml" if options[:thin]
     end
 
     # Customize the settings for the current location
     def edit_config_files
       edit_aerial_configuration
-      edit_thin_configuration if options[:thin]
     end
 
     def edit_aerial_configuration
@@ -95,15 +108,23 @@ module Aerial
       File.open("#{root}/config.yml", "w") { |f| f.puts config }
     end
 
-    def edit_thin_configuration
-      config = File.read("#{root}/thin.yml")
-      config.gsub! %r(/apps/aerial), root
-      File.open("#{root}/thin.yml", 'w') { |f| f.puts config }
-    end
-
     def copy(source, path = "../../config")
       cp_r(Pathname(__FILE__).dirname.join(path, source),
          root.join(File.basename(source).gsub(/\.sample/, "")))
+    end
+
+    def create_app_file
+      unless File.exist?(File.join(root, 'app.rb'))
+        File.open('app.rb', 'w') do |file|
+          file.puts "module Aerial"
+          file.puts "  class App < Sinatra::Base"
+          file.puts "    helpers do"
+          file.puts ""
+          file.puts "    end"
+          file.puts "  end"
+          file.puts "end"
+        end
+      end
     end
 
     def post_install_message
