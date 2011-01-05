7648a1c90b703b00f1cb31d951e41ecdbcd0fdc8
Matt Sears <matt@mattsears.com>
2011-01-05 09:26:03 -0500

    Refactored partial method to be more robust

--- lib/aerial/base.rb
+++ lib/aerial/base.rb
@@ -80,17 +80,32 @@ module Aerial
     end
 
     # Handy method to render partials including collections
-    def partial(template, options = {})
-      options.merge!(:layout => false)
-      return if options.has_key?(:collection) && options[:collection].nil?
+    # def partial(template, options = {})
+    #   options.merge!(:layout => false)
+    #   return if options.has_key?(:collection) && options[:collection].nil?
 
+    #   if collection = options.delete(:collection) then
+    #     collection.inject([]) do |buffer, member|
+    #       buffer << haml(template, options.merge(:layout => false,
+    #                                              :locals => {template.to_sym => member}))
+    #     end.join("\n")
+    #   else
+    #      haml(template, options)
+    #   end
+    # end
+
+    def partial(template, *args)
+      template_array = template.to_s.split('/')
+      template = template_array[0..-2].join('/') + "/_#{template_array[-1]}"
+      options = args.last.is_a?(Hash) ? args.pop : {}
+      options.merge!(:layout => false)
       if collection = options.delete(:collection) then
         collection.inject([]) do |buffer, member|
-          buffer << haml(template, options.merge(:layout => false,
-                                                 :locals => {template.to_sym => member}))
+          buffer << haml(:"#{template}", options.merge(:layout =>
+              false, :locals => {template_array[-1].to_sym => member}))
         end.join("\n")
       else
-         haml(template, options)
+        haml(:"#{template}", options)
       end
     end
 
